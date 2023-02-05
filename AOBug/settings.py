27f026d384ea09a2a66f444740ca586891b9982b
ismaely <7ilipe@gmail.com>
2023-02-05 10:14:06 +0100

    add model utilizador and create model genero

--- AOBug/settings.py
+++ AOBug/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'ajax',
     'index',
     'bug',
+    'utilizador',
 ]
 
 MIDDLEWARE = [
---
+++ utilizador/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Genero(models.Model):
+    nome = models.CharField(max_length=15)
+    sigla = models.CharField(max_length=1, blank=True, null=True)
+
+    def __str__(self):
+        return "%s" % (self.nome)
+
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+    sigla = models.CharField(max_length=10, blank=True, null=True)
+
+    def __str__(self):
+        return "%s" % (self.nome)
+
+# Model for User
+class Utilizador(models.Model):
+    user= models.ForeignKey(User, on_delete=models.CASCADE, parent_link=True)
+    categoria= models.ForeignKey(Categoria, on_delete=models.CASCADE, parent_link=True)
+    genero = models.ForeignKey(Genero, on_delete=models.CASCADE, parent_link=True)
+    data_nascimento=models.CharField(max_length=20,blank=True, null=True)
+    ndi = models.CharField(max_length=40,blank=True, null=True)
+    telefone = models.CharField(max_length=50,blank=True, null=True)
+    foto = models.ImageField(upload_to='user/%d-%m-%y/', blank=True, null=True, default="user.jpg")
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    
+    def __str__ (self):
+        return '%d' % (self.id)
