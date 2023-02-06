9e363c1a94f5077b6343689e80638a4f1983e29b
ismaely <7ilipe@gmail.com>
2023-02-06 13:30:44 +0100

    new fiie in model

--- bug/admin.py
+++ bug/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from bug.models import Categoria, Tipo, EstadoBug
+from bug.models import Categoria, Tipo, EstadoBug, Bug
 # Register your models here.
 
 class CategoriaAdmin(admin.ModelAdmin):
     list_display=('nome',)
 
+class BugAdmin(admin.ModelAdmin):
+    list_display=('autor','titulo','estado','categoria','dataPublicacao')
+
 admin.site.register(Categoria, CategoriaAdmin)
 admin.site.register(EstadoBug)
 admin.site.register(Tipo)
+admin.site.register(Bug,BugAdmin)
--- bug/models.py
+++ bug/models.py
@@ -20,13 +20,29 @@ class Tipo(models.Model):
         return "%s" % (self.nome)
 
 
+class Bug(models.Model):
+    titulo = models.CharField(max_length=200)
+    autor = models.ForeignKey(User, on_delete=models.PROTECT, parent_link=True)
+    estado = models.ForeignKey(EstadoBug, on_delete=models.PROTECT, parent_link=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING, parent_link=True)
+    tipo = models.ForeignKey(Tipo, on_delete=models.DO_NOTHING, parent_link=True)
+    dataPublicacao = models.DateField()
+    descricao = models.TextField()
+    #imagem = models.ImageField(upload_to="imagem/%d-%m-%yyyy/", blank=True, null=True)
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ['titulo']
+    def __str__ (self):
+        return self.id
 
 class Imagem(models.Model):
     titulo = models.CharField(max_length=170, blank=True, null=True)
-    descricao = models.CharField(max_length=500, blank=True, null=True)
-    arquivos = models.FileField(upload_to="uploads/%Y/")
+    descricao = models.TextField(blank=True, null=True)
+    arquivos = models.FileField(upload_to="uploads/%d-%m-%yyyy/",blank=True, null=True)
     created = models.DateTimeField(auto_now=True)
     updated = models.DateTimeField(auto_now_add=True)
+    bug = models.ForeignKey(Bug, on_delete=models.SET_NULL, null=True,parent_link=True)
 
     def __str__ (self):
         return self.id
