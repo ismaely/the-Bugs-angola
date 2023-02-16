33f0b339f9dd5c3d3d6854567bbf4bcfcd86e3e4
ismaely <7ilipe@gmail.com>
2023-02-16 20:46:35 +0100

    create model internacional

--- index/models.py
+++ index/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from datetime import date
+
 
 # Create your models here.
+class Internacional(models.Model):
+    titulo = models.CharField(max_length=200)
+    site = models.CharField(max_length=200, null=True, blank=True)
+    url = models.URLField(max_length=200, null=True, blank=True)
+    dataPublicacao = models.DateField()
+    descricao = models.TextField()
+    slug = models.SlugField(max_length=400,null=True, blank=True)
+    imagem = models.ImageField(upload_to="imagem/%d-%m-%yyyy/", blank=True, null=True)
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ['-created']
+
+    def __str__ (self):
+        return '%d' % self.id
+    
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.titulo+'-'+str(date.today()))
+        elif not self.slug:
+            self.slug = slugify(self.titulo+'-'+str(date.today()))
+
+        super(Internacional, self).save(*args, **kwargs)
