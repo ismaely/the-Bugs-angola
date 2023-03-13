6a2d5610e9a49694ee9f9121c6667498f91ecd0e
ismaely <7ilipe@gmail.com>
2023-03-13 11:08:51 +0100

    finish function for remove privilegio

--- ajax/views.py
+++ ajax/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User, Group, Permission
 from django.http import JsonResponse
 import random, json
 # Create your views here.
@@ -10,13 +11,24 @@ def remove_privilege_categoria(request):
             valor = []
             valor = request.body.decode('utf-8')
             valor = json.loads(valor)
-            groupo = valor['groupo']
-            perm = valor['lista_perm']
-            print(groupo)
-            print(perm)
+            grupo = valor['groupo']
+            listaPerm = valor['lista_perm']
+
+            if(len(valor['lista_perm']) > 1):
+                group = Group.objects.get(id=grupo)
+                for aux in listaPerm:
+                    perm = Permission.objects.get(id=aux)
+                    group.permissions.remove(perm)
+            else:
+                
+                aux = listaPerm.split('-') # o valor é exemplo #-23
+                group = Group.objects.get(id=grupo)
+                perm = Permission.objects.get(id=aux[1])
+                group.permissions.remove(perm)
+
             dados = {
-                '200':  True,
+                'resp':  True,
             }
         return JsonResponse(dados)
     except ValueError:
-        print("Não foi possivel eliminaar")
+        print("Não foi possivel eliminar permisão")
